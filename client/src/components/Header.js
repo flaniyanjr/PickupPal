f25909cfa9a310946695d8e82a29a980df26d3f9
Felix Laniyan <flaniyanjr@gmail.com>
2025-08-25 17:19:58 -0400

    created async function for logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,35 +1,34 @@
 import NavBar from "./NavBar"
 import header from '../styling/header.css'
 import newlogo from '../img/newlogo.jpg'
-import {Button} from '@mui/material';
+import { Button } from '@mui/material';
 
 import Signup from "./Signup";
 
-function Header({user, setUser}) {
+function Header({ user, setUser }) {
 
-    function handleLogout() {
-        fetch('/logout', {
-          method: 'DELETE'
-        }).then((resp) => {
-          if (resp.ok) {
-            setUser(null)
-          }
-        })
-      }
+  async function handleLogout() {
+    const response = await fetch('/logout', {
+      method: 'DELETE'
+    })
+    if (response.ok) {
+      setUser(null)
+    }
+  }
 
-    
-    return(
-        <div>
-            <NavBar />
-            <div>
-                {user ? <Button varient='contained' onClick={handleLogout} id= 'logout-button'>Logout</Button> : null}
-                {user ? <p className="user-info"><b>Welcome: {user.username}</b></p>: null}
-            </div>
-            <div className= 'logo-container'>
-                <img id="header-img" src={newlogo} alt="pickup pal logo"/>
-            </div>
-        </div>
-    )
+
+  return (
+    <div>
+      <NavBar />
+      <div>
+        {user ? <Button varient='contained' onClick={handleLogout} id='logout-button'>Logout</Button> : null}
+        {user ? <p className="user-info"><b>Welcome: {user.username}</b></p> : null}
+      </div>
+      <div className='logo-container'>
+        <img id="header-img" src={newlogo} alt="pickup pal logo" />
+      </div>
+    </div>
+  )
 }
 
 export default Header
