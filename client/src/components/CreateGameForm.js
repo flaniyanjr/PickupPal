1e3370486fe579f001886379cf9f4fbe9f84028f
Felix Laniyan <flaniyanjr@gmail.com>
2025-08-25 15:55:27 -0400

    created new async function

--- client/src/components/CreateGameForm.js
+++ client/src/components/CreateGameForm.js
@@ -1,12 +1,12 @@
-import { useOutletContext} from "react-router-dom";
-import {useState} from 'react'
+import { useOutletContext } from "react-router-dom";
+import { useState } from 'react'
 import CreatedGameCard from "./CreatedGameCard";
 import sportlogo4 from "../img/sportlogo4.jpg"
 import sportlogo5 from "../img/sportlogo5.jpeg"
 
 function CreateGame() {
 
-    const initialState= {
+    const initialState = {
         location: '',
         city: '',
         state: '',
@@ -16,156 +16,161 @@ function CreateGame() {
         image: ''
     }
 
-    const [gameData, setGameData]= useState(initialState)
-    const {addNewGame, createdGames, user}= useOutletContext()
+    const [gameData, setGameData] = useState(initialState)
+    const { addNewGame, createdGames, user } = useOutletContext()
 
-    const currentDate= new Date() 
-    let day= currentDate.getDate()
-    let month= currentDate.getMonth() + 1
-    const year= currentDate.getFullYear()
+    const currentDate = new Date()
+    let day = currentDate.getDate()
+    let month = currentDate.getMonth() + 1
+    const year = currentDate.getFullYear()
 
     if (day < 10) {
         day = '0' + day;
-     }
+    }
 
-     if (month < 10) {
+    if (month < 10) {
         month = '0' + month;
-     } 
+    }
 
     const today = year + '-' + month + '-' + day;
 
     let createdGameCards
 
     if (createdGames) {
-        createdGameCards= createdGames.map(gameObj => {
-            return <CreatedGameCard key= {gameObj.id} gameObj= {gameObj}/>
-        })
-    } 
-
-    function handleChange(e) {
-        setGameData(current => {
-            return {...current, [e.target.name] : e.target.value}
+        createdGameCards = createdGames.map(gameObj => {
+            return <CreatedGameCard key={gameObj.id} gameObj={gameObj} />
         })
     }
 
-    function handleSubmit(e) {
+    function handleChange(e) {
+        setGameData(current => {
+            return { ...current, [e.target.name]: e.target.value }
+        })
+    }
+
+    async function handleSubmit(e) {
         e.preventDefault()
         if (gameData['location'] === '' || gameData['city'] === '' || gameData['state'] === '' || gameData['date'] === '' || gameData['time'] === '') {
             alert('All fields except for image are required')
         } else {
-            fetch('/pickup_games', {
-                method: 'POST',
-                headers: {'Content-Type' : 'application/json'},
-                body: JSON.stringify({...gameData, total_attendees: 0})
-            })
-            .then(r => r.json())
-            .then(newGame => {
+            try {
+                const response = await fetch('/pickup_games', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ ...gameData, total_attendees: 0 })
+                })
+                if (!response.ok) {
+                    throw new Error(`Failed to create game. Status: ${response.status}`)
+                }
+                const newGame = await response.json()
                 addNewGame(newGame)
-            })
-            setGameData(initialState)
+                setGameData(initialState)
+            } catch (error) {
+                console.error('Error submitting new game:', error)
+            }
         }
     }
 
-    return(
+    return (
         user ?
-        <div>
-            <div className= "create-game-form">
-                <h4>Create a Game</h4>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label>Venue</label>
-                        <input type= 'text' name= 'location' value= {gameData.location} onChange= {handleChange}/>
+            <div>
+                <div className="create-game-form">
+                    <h4>Create a Game</h4>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label>Venue</label>
+                            <input type='text' name='location' value={gameData.location} onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label>City</label>
+                            <input type='text' name='city' value={gameData.city} onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label>State</label>
+                            <select name="state" onChange={handleChange} value={gameData.state}>
+                                <option value="" selected="selected" ></option>
+                                <option value="AL">Alabama</option>
+                                <option value="AK">Alaska</option>
+                                <option value="AZ">Arizona</option>
+                                <option value="AR">Arkansas</option>
+                                <option value="CA">California</option>
+                                <option value="CO">Colorado</option>
+                                <option value="CT">Connecticut</option>
+                                <option value="DE">Delaware</option>
+                                <option value="DC">District of Columbia</option>
+                                <option value="FL">Florida</option>
+                                <option value="GA">Georgia</option>
+                                <option value="HI">Hawaii</option>
+                                <option value="ID">Idaho</option>
+                                <option value="IL">Illinois</option>
+                                <option value="IN">Indiana</option>
+                                <option value="IA">Iowa</option>
+                                <option value="KS">Kansas</option>
+                                <option value="KY">Kentucky</option>
+                                <option value="LA">Louisiana</option>
+                                <option value="ME">Maine</option>
+                                <option value="MD">Maryland</option>
+                                <option value="MA">Massachusetts</option>
+                                <option value="MI">Michigan</option>
+                                <option value="MN">Minnesota</option>
+                                <option value="MS">Mississippi</option>
+                                <option value="MO">Missouri</option>
+                                <option value="MT">Montana</option>
+                                <option value="NE">Nebraska</option>
+                                <option value="NV">Nevada</option>
+                                <option value="NH">New Hampshire</option>
+                                <option value="NJ">New Jersey</option>
+                                <option value="NM">New Mexico</option>
+                                <option value="NY">New York</option>
+                                <option value="NC">North Carolina</option>
+                                <option value="ND">North Dakota</option>
+                                <option value="OH">Ohio</option>
+                                <option value="OK">Oklahoma</option>
+                                <option value="OR">Oregon</option>
+                                <option value="PA">Pennsylvania</option>
+                                <option value="RI">Rhode Island</option>
+                                <option value="SC">South Carolina</option>
+                                <option value="SD">South Dakota</option>
+                                <option value="TN">Tennessee</option>
+                                <option value="TX">Texas</option>
+                                <option value="UT">Utah</option>
+                                <option value="VT">Vermont</option>
+                                <option value="VA">Virginia</option>
+                                <option value="WA">Washington</option>
+                                <option value="WV">West Virginia</option>
+                                <option value="WI">Wisconsin</option>
+                                <option value="WY">Wyoming</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label>Date</label>
+                            <input type='date' name='date' value={gameData.date} min={today} onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label>Time</label>
+                            <input type='time' name='time' value={gameData.time} onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label>Sport</label>
+                            <input type='text' name='sport' value={gameData.sport} onChange={handleChange} />
+                        </div>
+                        <div>
+                            <label>Image</label>
+                            <input type='text' name='image' value={gameData.image} onChange={handleChange} />
+                        </div>
+                        <button className='submit-button green' type='submit'>Create</button>
+                    </form>
+                    <div className='logo-four-image-containter'>
+                        <img id='logo-four' src={sportlogo4} alt='tennis sports logo' />
                     </div>
-                    <div>
-                        <label>City</label>
-                        <input type= 'text' name='city' value={gameData.city} onChange={handleChange}/>
+                    <div className='logo-five-image-containter'>
+                        <img id='logo-five' src={sportlogo5} alt='tennis sports logo' />
                     </div>
-                    <div>
-                        <label>State</label>
-                        <select name="state" onChange={handleChange} value={gameData.state}>
-                            <option value="" selected="selected" ></option>
-                            <option value="AL">Alabama</option>
-                            <option value="AK">Alaska</option>
-                            <option value="AZ">Arizona</option>
-                            <option value="AR">Arkansas</option>
-                            <option value="CA">California</option>
-                            <option value="CO">Colorado</option>
-                            <option value="CT">Connecticut</option>
-                            <option value="DE">Delaware</option>
-                            <option value="DC">District of Columbia</option>
-                            <option value="FL">Florida</option>
-                            <option value="GA">Georgia</option>
-                            <option value="HI">Hawaii</option>
-                            <option value="ID">Idaho</option>
-                            <option value="IL">Illinois</option>
-                            <option value="IN">Indiana</option>
-                            <option value="IA">Iowa</option>
-                            <option value="KS">Kansas</option>
-                            <option value="KY">Kentucky</option>
-                            <option value="LA">Louisiana</option>
-                            <option value="ME">Maine</option>
-                            <option value="MD">Maryland</option>
-                            <option value="MA">Massachusetts</option>
-                            <option value="MI">Michigan</option>
-                            <option value="MN">Minnesota</option>
-                            <option value="MS">Mississippi</option>
-                            <option value="MO">Missouri</option>
-                            <option value="MT">Montana</option>
-                            <option value="NE">Nebraska</option>
-                            <option value="NV">Nevada</option>
-                            <option value="NH">New Hampshire</option>
-                            <option value="NJ">New Jersey</option>
-                            <option value="NM">New Mexico</option>
-                            <option value="NY">New York</option>
-                            <option value="NC">North Carolina</option>
-                            <option value="ND">North Dakota</option>
-                            <option value="OH">Ohio</option>
-                            <option value="OK">Oklahoma</option>
-                            <option value="OR">Oregon</option>
-                            <option value="PA">Pennsylvania</option>
-                            <option value="RI">Rhode Island</option>
-                            <option value="SC">South Carolina</option>
-                            <option value="SD">South Dakota</option>
-                            <option value="TN">Tennessee</option>
-                            <option value="TX">Texas</option>
-                            <option value="UT">Utah</option>
-                            <option value="VT">Vermont</option>
-                            <option value="VA">Virginia</option>
-                            <option value="WA">Washington</option>
-                            <option value="WV">West Virginia</option>
-                            <option value="WI">Wisconsin</option>
-                            <option value="WY">Wyoming</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label>Date</label>
-                        <input type= 'date' name='date' value={gameData.date} min={today} onChange={handleChange}/>
-                    </div>
-                    <div>
-                        <label>Time</label>
-                        <input type= 'time' name='time' value={gameData.time} onChange={handleChange}/>
-                    </div>
-                    <div>
-                        <label>Sport</label>
-                        <input type= 'text' name='sport' value={gameData.sport} onChange={handleChange}/>
-                    </div>
-                    <div>
-                        <label>Image</label>
-                        <input type= 'text' name='image' value={gameData.image} onChange={handleChange}/>
-                    </div>
-                    <button className= 'submit-button green' type='submit'>Create</button>
-                </form>
-                <div className= 'logo-four-image-containter'>
-                    <img id= 'logo-four' src= {sportlogo4} alt= 'tennis sports logo' />
                 </div>
-                <div className= 'logo-five-image-containter'>
-                <img id= 'logo-five' src= {sportlogo5} alt= 'tennis sports logo' /> 
-                </div>
+                {createdGameCards}
             </div>
-            {createdGameCards}
-        </div>
-        :
-        <h1 className="required-login-message">Login required to access this page </h1>
+            :
+            <h1 className="required-login-message">Login required to access this page </h1>
     )
 }
 
--- client/src/components/CreatedGameCard.js
+++ client/src/components/CreatedGameCard.js
@@ -1,30 +1,37 @@
-import { useOutletContext} from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 import { useState } from "react";
 
-function CreatedGameCard({gameObj}) {
-    const {location, city, state, date, time, sport, image, total_attendees, id}= gameObj
+function CreatedGameCard({ gameObj }) {
+    const { location, city, state, date, time, sport, image, total_attendees, id } = gameObj
 
-    const {deleteNewGame, allGames, updateNewGame, userSignups, removeSignup}= useOutletContext()
+    const { deleteNewGame, allGames, updateNewGame, userSignups, removeSignup } = useOutletContext()
 
-    const [newDate, setNewDate]= useState(date)
-    const [newTime, setNewTime]= useState(time)
-    const [showUpdate, setShowUpdate]= useState(false)
-    const [submitted, setSubmitted]= useState(false)
+    const [newDate, setNewDate] = useState(date)
+    const [newTime, setNewTime] = useState(time)
+    const [showUpdate, setShowUpdate] = useState(false)
+    const [submitted, setSubmitted] = useState(false)
 
-    const currentGameSignupsArray= userSignups.filter(signup => {
+    const currentGameSignupsArray = userSignups.filter(signup => {
         return signup.pickup_game_id === id
     })
 
-    const currentGameSignup= currentGameSignupsArray[0]
+    const currentGameSignup = currentGameSignupsArray[0]
 
 
-    function handleDelete() {
-        fetch(`/pickup_games/${id}`, {
-            method: 'DELETE',
-        })
-        deleteNewGame(id)
-        if (currentGameSignup) {
-            removeSignup(currentGameSignup.id)
+    async function handleDelete() {
+        try {
+            const response = await fetch(`/pickup_games/${id}`, {
+                method: 'DELETE',
+            })
+            if (!response.ok) {
+                throw new Error(`Failed to delete game. Status Code: ${response.status}`)
+            }
+            deleteNewGame(id)
+            if (currentGameSignup) {
+                removeSignup(currentGameSignup.id)
+            }
+        } catch (error) {
+            console.log('Error deleting game:', error)
         }
     }
 
@@ -40,14 +47,14 @@ function CreatedGameCard({gameObj}) {
         e.preventDefault()
         fetch(`/pickup_games/${id}`, {
             method: 'PATCH',
-            headers: {'Content-Type' : 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 date: newDate,
                 time: newTime
             })
         })
-        .then(r => r.json())
-        .then(newGame => updateNewGame(newGame))
+            .then(r => r.json())
+            .then(newGame => updateNewGame(newGame))
         setSubmitted(true)
         setShowUpdate(false)
     }
@@ -57,31 +64,31 @@ function CreatedGameCard({gameObj}) {
         setSubmitted(false)
     }
 
-    const targetAttendeeArray= allGames.filter(game => {
+    const targetAttendeeArray = allGames.filter(game => {
         return game.id === id
     })
-    const targetAttendeeGame= targetAttendeeArray[0]
-    const targetAttendeeTotal= targetAttendeeGame.total_attendees
+    const targetAttendeeGame = targetAttendeeArray[0]
+    const targetAttendeeTotal = targetAttendeeGame.total_attendees
 
-    const currentDate= new Date() 
-    let day= currentDate.getDate()
-    let month= currentDate.getMonth() + 1
-    const year= currentDate.getFullYear()
+    const currentDate = new Date()
+    let day = currentDate.getDate()
+    let month = currentDate.getMonth() + 1
+    const year = currentDate.getFullYear()
 
     if (day < 10) {
         day = '0' + day;
-     }
+    }
 
-     if (month < 10) {
+    if (month < 10) {
         month = '0' + month;
-     } 
+    }
 
     const today = year + '-' + month + '-' + day;
 
-    return(
-        <div className= "card">
+    return (
+        <div className="card">
             <h2>{location}</h2>
-            <img className= "location-image submit-button" src= {image} alt= {location} onClick= {handleShowUpdate}/>
+            <img className="location-image submit-button" src={image} alt={location} onClick={handleShowUpdate} />
             <div>
                 <p> City: {city}</p>
                 <p> State: {state}</p>
@@ -90,22 +97,22 @@ function CreatedGameCard({gameObj}) {
                 <p> Sport: {sport}</p>
                 <p> Total Attendees: {targetAttendeeTotal}</p>
             </div>
-            <button className= 'submit-button red' onClick={handleDelete}>Delete</button>
+            <button className='submit-button red' onClick={handleDelete}>Delete</button>
             {submitted ? <p>Updated!</p> : null}
-            {showUpdate ? 
+            {showUpdate ?
                 <form onSubmit={handleSubmit}>
                     <div>
-                        <label id= 'update-date-text'>Update Date</label>
-                        <input id= 'update-date-input' type= 'date' name='date' value= {newDate} min={today} onChange={handleDateChange}/>
+                        <label id='update-date-text'>Update Date</label>
+                        <input id='update-date-input' type='date' name='date' value={newDate} min={today} onChange={handleDateChange} />
                     </div>
                     <div>
-                        <label id= 'update-time-text'>Update Time</label>
-                        <input id= 'update-time-input' type= 'time' name='time' value={newTime} onChange={handleTimeChange}/>
+                        <label id='update-time-text'>Update Time</label>
+                        <input id='update-time-input' type='time' name='time' value={newTime} onChange={handleTimeChange} />
                     </div>
                     <button className='submit-button green' type='submit'>Submit</button>
-            </form>
-            : null}
-            
+                </form>
+                : null}
+
         </div>
     )
 }
