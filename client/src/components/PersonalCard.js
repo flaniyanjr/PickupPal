e8a64f544761e29604e55352ca686b93acd3ec52
Felix Laniyan <flaniyanjr@gmail.com>
2025-08-27 16:49:38 -0400

    updated another function to async

--- client/src/components/PersonalCard.js
+++ client/src/components/PersonalCard.js
@@ -1,47 +1,60 @@
 import { useOutletContext } from "react-router-dom";
 
-function PersonalCard({gameObj}) {
+function PersonalCard({ gameObj }) {
 
-    const {userSignups, removeSignup, allGames, updateGameAttendees}= useOutletContext()
+    const { userSignups, removeSignup, allGames, updateGameAttendees } = useOutletContext()
 
-    const {location, city, state, date, time, sport, image, total_attendees, id}= gameObj
+    const { location, city, state, date, time, sport, image, total_attendees, id } = gameObj
 
-    const targetSignupArray= userSignups.filter(signup => {
+    const targetSignupArray = userSignups.filter(signup => {
         if (signup.pickup_game.id === id) {
             return true
         } else {
             return false
         }
     })
-    const targetSignup= targetSignupArray[0]
+    const targetSignup = targetSignupArray[0]
 
 
-    const targetAttendeeArray= allGames.filter(game => {
+    const targetAttendeeArray = allGames.filter(game => {
         return game.id === id
     })
-    const targetAttendeeGame= targetAttendeeArray[0]
-    const targetAttendeeTotal= targetAttendeeGame.total_attendees
+    const targetAttendeeGame = targetAttendeeArray[0]
+    const targetAttendeeTotal = targetAttendeeGame.total_attendees
 
-    function handleDelete() {
-        fetch(`/player_signups/${targetSignup.id}`, {
-            method: 'DELETE'
-        })
-        removeSignup(targetSignup.id)
-        fetch(`/pickup_games/${id}`, {
-            method: 'PATCH',
-            headers: {'Content-Type' : 'application/json'},
-            body: JSON.stringify({
-                total_attendees: targetAttendeeTotal - 1
+    async function handleDelete() {
+        try {
+            const responseOne = await fetch(`/player_signups/${targetSignup.id}`, {
+                method: 'DELETE'
             })
-        })
-        .then(r => r.json())
-        .then(newGame => updateGameAttendees(newGame))
+            if (responseOne.ok) {
+                removeSignup(targetSignup.id)
+                const responseTwo = await fetch(`/pickup_games/${id}`, {
+                    method: 'PATCH',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        total_attendees: targetAttendeeTotal - 1
+                    })
+                })
+                if (responseTwo.ok) {
+                    const newGame = await responseTwo.json()
+                    updateGameAttendees(newGame)
+                } else {
+                    throw new Error(`Failed to decrease attendance number. Status: ${responseTwo.status}`)
+                }
+            } else {
+                throw new Error(`Failed to delete signup. Status: ${responseOne.status}`)
+            }
+        } catch (error) {
+            console.error('Error:', error)
+        }
+
     }
 
-    return(
-        <div className= 'card'>
+    return (
+        <div className='card'>
             <h2>{location}</h2>
-            <img className= "location-image" src= {image} alt= {location}/>
+            <img className="location-image" src={image} alt={location} />
             <div>
                 <p> City: {city}</p>
                 <p> State: {state}</p>
